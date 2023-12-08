c18e528a4ee2864b225b713ae11515c1ae16dd7e
TylerMS887 <tylermageeshields@gmail.com>
2023-12-08 08:18:30 +0000

    add about template

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask, os, time
 
+version = "1.0"
 app = flask.Flask(__name__)
 app.config['EXPLAIN_TEMPLATE_LOADING'] = True
 
@@ -7,6 +8,20 @@ app.config['EXPLAIN_TEMPLATE_LOADING'] = True
 def index():
     # Set the Content-Type header to HTML
     response = app.make_response(flask.render_template('index.html'))
+    response.headers['Content-Type'] = 'text/html; charset=utf-8'
+    return response
+
+@app.route("/explained")
+def explained():
+    # Set the Content-Type header to HTML
+    response = app.make_response(flask.render_template('explained.html'))
+    response.headers['Content-Type'] = 'text/html; charset=utf-8'
+    return response
+
+@app.route("/about")
+def about():
+    # Set the Content-Type header to HTML
+    response = app.make_response(flask.render_template('about.html'))
     response.headers['Content-Type'] = 'text/html; charset=utf-8'
     return response
 
@@ -35,11 +50,14 @@ def query():
     return flask.request.args.get('q', None)
 def results():
     return '<b>bold</b>'
+def pruneversion():
+    return version
 
 app.jinja_env.globals.update(pid=pid)
 app.jinja_env.globals.update(unixtime=unixtime)
 app.jinja_env.globals.update(query=query)
 app.jinja_env.globals.update(results=results)
+app.jinja_env.globals.update(pruneversion=pruneversion)
 
 if __name__ == "__main__":
     app.run(debug=True)
